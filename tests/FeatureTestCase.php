a667ca292db2097f386a31b61460156b9a11274d
Leandro Gorriz <lealp88@gmail.com>
2016-12-16 16:00:02 -0300

    Add feature test case

---
+++ tests/FeatureTestCase.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: lgorriz
+ * Date: 16/12/16
+ * Time: 15:08
+ */
+
+use \Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class FeatureTestCase extends TestCase
+{
+    use DatabaseTransactions;
+}
--- tests/features/ExampleTest.php
+++ tests/features/ExampleTest.php
@@ -2,12 +2,12 @@
 
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class ExampleTest extends TestCase
+
+class ExampleTest extends FeatureTestCase
 {
 
-    use DatabaseTransactions;
+    
     /**
      * A basic functional test example.
      *
